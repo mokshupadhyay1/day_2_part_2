45412e8852e5c94f8aad25a9054c2542be5eab3e
mokshupadhyay1 <drumsharma9837@gmail.com>
2026-01-19 14:52:26 +0530

    Initial commit

---
+++ src/main/java/com/example/day_2_part_2/Day2Part2Application.java
@@ -0,0 +1,13 @@
+package com.example.day_2_part_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2Part2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day2Part2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/day_2_part_2/controller/Home.java
@@ -0,0 +1,4 @@
+package com.example.day_2_part_2.controller;
+
+public class Home {
+}
